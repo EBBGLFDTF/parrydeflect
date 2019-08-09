595e714a0bde26d6c5304a1a4b363ce6c14854c1
PitEG <enriqueiigan@gmail.com>
2019-08-08 20:15:07 -0700

    Restarted

---
+++ parrydeflect/Assets/Scripts/GameState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState {
+	private static GameState instance;
+
+	int points = 0;
+
+	public GameState Instance {
+		get { return instance; }	
+	}
+
+	//Calling this constructor effectively resets the game state
+	public GameState() {
+		instance = new GameState();
+	}
+
+
+}
---
+++ parrydeflect/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+	Rigidbody2D rigidbody;
+
+	//movement
+	public float speed = 1.0f;
+
+	private void Awake() {
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+       rigidbody = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Move();
+    }
+
+	private void Move() {
+		float xMovement = Input.GetAxisRaw("Horizontal");
+		float yMovement = Input.GetAxisRaw("Vertical");
+
+		Vector2 direction = new Vector2(xMovement, yMovement);
+		direction = direction.normalized;
+		
+		Vector2 newPosition = rigidbody.position + (direction * this.speed);
+		rigidbody.MovePosition(newPosition);
+	}
+
+	private void ResetGame() {
+		new GameState();
+	}
+}
